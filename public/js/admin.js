942fa6833e137041a8a2ee1238ad779971f8fed9
kjh957 <wlsgh957@naver.com>
2019-01-15 14:36:13 +0900

    쇼핑몰15일

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,5 @@
+// database = CRUD(Create/ Read/ Update/ Delete)
+
 // Firebase Init
 var config = {
     apiKey: "AIzaSyBf8yvKxFZJNwL6UP1Y9BS5G85crVxBzM4",
@@ -8,24 +10,128 @@ var config = {
     messagingSenderId: "16738569239"
   };
   firebase.initializeApp(config);
-  //공통 변수
-  var db = firebase.database();// firebase의 데이터베이스 객체
-  var ref = null;
-  var key = "";
+  
+  // 공통 변수
+var db = firebase.database();	//firebase의 데이터베이스 객체
+var ref = new Array();
+var key = "";
 
-  //네비게이션 구현
-  $(".navs li").click(function(){
-    $(".navs li").css({"color":"#aaa", "background-color":"transparent"});
-    $(this).css({"color":"#fff", "background-color":"#444"});
+// 네비게이션 구현
+$(".navs li").on("click", function(){
+	$(".navs li").css({"color":"#aaa", "background-color":"transparent"});
+	$(this).css({"color":"#fff", "background-color":"#444"});
+});
+$(".navs li").eq(0).trigger("click");
+
+// 페이지 시작과 동시에 homeInit()실행하고, FB이벤트 콜백함수를 생성한다.
+homeInit();
+function homeInit() {
+	ref[0] = db.ref("root/home/");
+	ref[0].on("child_added", homeAdd);
+  ref[0].on("child_removed", homeRev);
+  ref[0].on("child_changed", homeChg)
+}
+/****  Firebase Callback 함수, 시작 ****/
+// Home 데이터가 추가되면 data변수에 추가된 내용을 담아 실행한다.
+function homeAdd(data){
+	var sel = ["", ""];
+	if(data.val().target == "_blank") sel[0] = "selected";
+	else sel[1] = "selected"; 
+	var html = `
+	<li id="${data.key}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block home_title" 
+		value="${data.val().title}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block home_link" 
+		value="${data.val().link}">
+		<select class="w3-select w3-border w3-show-inline-block home_target">
+			<option value="_blank" ${sel[0]}>새창</option>
+			<option value="_self"  ${sel[1]}>현재창</option>
+		</select>
+		<button class="w3-green w3-button onclick="dataChg(this);">수정</button>
+		<button class="w3-red w3-button" onclick="dataRev(this);">삭제</button>
+	</li>`;
+	$("#homes").append(html);
+}
+// Home 데이터가 삭제되면 data변수에 삭제된 내용을 담아 실행한다.
+function homeRev(data) {
+	$("#"+data.key).remove();
+}
+// Home 데이터가 삭제되면 data변수에 수정된 내용을 담아 실행한다.
+function homeChg(data) {
+  $("#"+data.key).stop().animate({"opacity":0}, 300, function(){
+    $(this).css({"opacity":1});
   });
-  $(".navs li").eq(0).trigger("click");
+}
+/****  Firebase Callback 함수, 종료 ****/
 
- //page1 / HOME
+
+// home 저장 버튼을 눌렀을때.
+$("#bt1_save").on("click", function(){
+	if($("#title1").val() == "") {
+		alert("제목 !!");
+		return;
+	}
+	if($("#link1").val() == "") {
+		alert("링크 !!");
+		return;
+  }
+  //Firebase 추가 명령
+	ref[0].push({
+		title: $("#title1").val(),
+		link: $("#link1").val(),
+		target: $("#target1").val()
+	}).key;
+	$("#title1").val("");
+	$("#link1").val("");
+});
+// home 삭제버튼을 눌렀을때
+function dataRev(obj) {
+	if(confirm("정말로 삭제하시겠습니까?")) {
+    //Firebase 삭제 명령
+		db.ref("root/home/"+$(obj).parent().attr("id")).remove();
+	}
+}
+// home 수정버튼을 눌렀을때
+function dataChg(obj){
+  var $li = $(obj).parent();
+  key = $li.attr("id");
+  if($li.find(".home_title").val() == "") {
+		alert("제목 !!");
+		return;
+	}
+	if($li.find(".home_link").val() == "") {
+		alert("링크 !!");
+		return;
+  }
+  //Firebase 수정 명령
+  db.ref("root/home/"+key).update({
+    title:$li.find(".home_title").val(),
+    link:$li.find(".home_link").val(),
+    target:$li.find(".home_target").val(),
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//page1 / HOME
+/*
 (function(){
 	ref = db.ref("root/home");
 	ref.on("child_added", homeAdd);
-  }());
-  $("#bt1_save").click(function(){
+}());
+$("#bt1_save").click(function(){
 	var $title = $("#title1");
 	var $link = $("#link1");
 	var $target = $("#target1");
@@ -37,28 +143,31 @@ var config = {
 		alert("링크주소를 입력하세요.");
 		return;
 	}
-      ref.push({
-          title: $title.val(),
-          link: $link.val(),
-          target: $target.val()
-      }).key;
-      $title.val('');
-      $link.val('');
-  });
-  function homeAdd(data){
-    var sel = ['',''];
-    if(data.val().target == "_blank") sel[0] = "selected";
-    else sel[1] = "selected";
-      var html = `
-      <div id="${data.id}">
-            <input type="text" class="w3-input w3-border w3-show-inline-block" value="${data.val().title}">
-            <input type="text" class="w3-input w3-border w3-show-inline-block" value="${data.val().link}">
-            <select class="w3-select w3-show-inline-block w3-border">
-                <option value="_blank" ${sel[0]}>새창</option>
-                <option value="_self" ${sel[1]}>현재창</option>
-            </select>
-            <button type="button" id="bt1_save" class="w3-indigo w3-button">수정</button>
-            <button type="button" class="w3-red w3-button">삭제</button>
-      </div>`;
-      $("#homes").append(html);
-  }
+	ref.push({
+		title: $title.val(),
+		link: $link.val(),
+		target: $target.val()
+	}).key;
+	$title.val('');
+	$link.val('');
+});
+function homeAdd(data) {
+	var sel = ['', ''];
+	if(data.val().target == "_blank") sel[0] = "selected";
+	else sel[1] = "selected";
+	var html = `
+	<div id="${data.id}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block" 
+		value="${data.val().title}">
+		<input type="text" class="w3-input w3-border w3-show-inline-block" 
+		value="${data.val().link}">
+		<select class="w3-select w3-border w3-show-inline-block">
+			<option value="_blank" ${sel[0]}>새창</option>
+			<option value="_self" ${sel[1]}>현재창</option>
+		</select>
+		<button type="button" class="w3-indigo w3-button">수정</button>
+		<button type="button" class="w3-red w3-button">삭제</button>
+	</div>`;
+	$("#homes").append(html);
+}
+*/
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+const log = console.log;
 // Firebase Init
 var config = {
     apiKey: "AIzaSyBf8yvKxFZJNwL6UP1Y9BS5G85crVxBzM4",
@@ -8,6 +9,61 @@ var config = {
     messagingSenderId: "16738569239"
   };
   firebase.initializeApp(config);
+//Firebase Init
+var db = firebase.database();
+
+//카테고리 HOME 생성
+db.ref("root/home").on("child_added", homeAdd);
+function homeAdd(data) {
+	var html = `
+	<li class="rt_arrow">
+		<a href="${data.val().link}" target="${data.val().target}">${data.val().title}</a>
+	</li>`;
+	$(".nav_sub").eq(0).append(html);
+}
+
+// 카테고리 SHOP 생성 - Ajax/json 통신
+$.ajax({
+	type: "get",
+	url: "../json/shop.json",
+	data: {},
+	dataType: "json",
+	success: function(data) {
+		var html = `<div class="shop_cates wrap clear">`;
+		for(var i=0; i<data.cates.length; i++){
+			html += `<ul>
+			<li class="shop_cate_tit">${data.cates[i].tit}</li>
+			<li>
+				<ul class="shop_cate_names">`;
+				for(var j=0; j<data.cates[i].data.length; j++) {
+					html += `
+					<li class="shop_cate_name rt_arrow">
+					<a href="${data.cates[i].data[j].link}" target="${data.cates[i].data[j].target}">
+					${data.cates[i].data[j].name}</a>
+					</li>`;
+				}
+			html +=	`</ul></li></ul>`;
+		}
+		html += `</div><ul class="shop_prds wrap clear">`;
+		for(var i=0; i<data.prds.length; i++) {
+			html += `<li class="shop_prd">
+				<a href="${data.prds[i].link}" target="${data.prds[i].target}">
+				<img src="${data.prds[i].src}" class="img">
+				</a>
+			</li>`;
+		}
+		html += `</ul>`;
+		$(".nav_sub").eq(1).append(html);
+	},
+	error: function(xhr) {
+		log(xhr);
+	}
+});
+
+// window.resize()구현 
+$(window).resize(function(){
+	
+}).trigger("resize");
 
 // top_nav hover 이벤트
 $(".top_icon").mouseenter(function(){
@@ -15,6 +71,16 @@ $(".top_icon").mouseenter(function(){
 });
 $(".top_icon").mouseleave(function(){
 	$(this).children("img").css({"opacity":1});
+});
+
+// nav 이벤트(nav_sub show/hide)
+$(".nav").mouseenter(function(){
+	$(this).children(".nav_sub").css({"display":"block", "opacity":0}).stop().animate({"opacity":1, "top":"45px"}, 200);
+});
+$(".nav").mouseleave(function(){
+	$(this).children(".nav_sub").stop().animate({"opacity":0, "top":"80px"},200, function(){
+		$(this).css({"display":"none"});
+	});
 });
 
 // rt_wings 이벤트
